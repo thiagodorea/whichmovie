2056c222b57910f6439be790f6b47cae91c3d709
thiagodorea <thiago.dorea@hotmail.com>
2022-01-12 00:54:53 -0300

    Project Created

---
+++ backend/src/main/java/com/tdorea/whichmovie/WhichmovieApplication.java
@@ -0,0 +1,13 @@
+package com.tdorea.whichmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhichmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tdorea/whichmovie/WhichmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdorea.whichmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhichmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
